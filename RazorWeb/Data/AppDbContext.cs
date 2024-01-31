2c6c5c082942d2d6b29bf1e9e00f6f21abeb7376
b-white-70 <brian.d.white@live.com>
2024-01-30 23:15:34 -0500

    first commit

---
+++ RazorWeb/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define DbSet properties for your entities here
+    }
+}
